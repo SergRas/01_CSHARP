b21bf419a2d1c6a89f27284530ca31e991b1dcda
SergRas <rasulov_sf@mail.ru>
2024-03-20 20:14:03 +0500

    перенесли задачи в lesson1

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 10;
+int b = 2;
+int c = 15;
+int d = 9;
+int e = 3;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
