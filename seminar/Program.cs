097c4104293585d9c8a1cde0ac0edd1d538cdb92
SergRas <rasulov_sf@mail.ru>
2024-03-30 09:46:41 +0500

    add seminar

---
+++ seminar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+ 
+        while (true)
+        {
+            Console.Write("Введите число или 'q' для выхода: ");
+            string input = Console.ReadLine()!;
+              int number =Convert.ToInt32(input);
+            if (input == "q")
+            {
+                break;
+            }
+
+            
+             
+                int sum = 0;
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine("Сумма цифр введенного числа четная выход.");
+                    break;
+                }
+            
+            else
+            {
+                Console.WriteLine("Сумма цифр введенного числа нечетная вводите ещё");
+            }
+        }
