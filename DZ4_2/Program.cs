459ed8e7c4d7fb5a1a3d50c42b9f60cd520671e1
SergRas <rasulov_sf@mail.ru>
2024-04-02 22:51:36 +0500

    add DZ4_2

---
+++ DZ4_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+partial class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int[] array = new int[10]; // Задаем размер массива
+
+        // Заполняем массив случайными трехзначными числами
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(100, 1000); // Генерируем трехзначные числа
+        }
+
+        Console.WriteLine("Сгенерированный массив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        int count = CountEvenNumbers(array);
+        Console.WriteLine($"Количество четных чисел в массиве: {count}");
+    }
+
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+        foreach (int num in array)
+        {
+            if (num % 2 == 0) // Проверяем, является ли число четным
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
