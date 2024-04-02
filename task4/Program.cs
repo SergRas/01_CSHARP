329e9e74ddd18fbb34d8d8a2aef307c2f9395971
SergRas <rasulov_sf@mail.ru>
2024-04-02 22:53:28 +0500

    add task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,25 +1,32 @@
-﻿int a = 10;
-int b = 2;
-int c = 15;
-int d = 9;
-int e = 3;
+﻿using System;
 
-int max = a;
+class Program
+{
+    static void Main()
+    {
+        int[,] numbers = new int[,] {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
 
-if (b > max)
-{
-    max = b;
+        int x = 2;
+        int y = 2;
+
+        int numRows = numbers.GetLength(0);
+        int numCols = numbers.GetLength(1);
+
+        if (x >= 1 && x <= numRows && y >= 1 && y <= numCols)
+        {
+            Console.WriteLine("Значение элемента в позиции ({0},{1}) равно {2}", x, y, numbers[x-1, y-1]);
+        }
+        else if (x < 1 || x > numRows)
+        {
+            Console.WriteLine("Позиция по рядам выходит за пределы массива");
+        }
+        else
+        {
+            Console.WriteLine("Позиция по колонкам выходит за пределы массива");
+        }
+    }
 }
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (e > max)
-{
-    max = e;
-}
-Console.WriteLine(max);
