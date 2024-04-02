f37187078afa199c9fced03a11b7a658a3a95a89
SergRas <rasulov_sf@mail.ru>
2024-04-02 22:51:51 +0500

    add DZ4_3

---
+++ DZ4_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        ReverseArray(array);
+
+        Console.WriteLine("\nПеревернутый массив:");
+        PrintArray(array);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left < right)
+        {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
