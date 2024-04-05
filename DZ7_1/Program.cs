5f1c99710f16714c21a48c5983b22f5d3a334466
SergRas <rasulov_sf@mail.ru>
2024-04-06 00:14:21 +0500

    add dz7_1 promatt

---
+++ DZ7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от M до N . Не использовать циклы,
+// использовать рекурсию.
+
+string Len(int start, int stop)
+{
+        if (start <= stop)
+        {
+            return start + " " + Len(start + 1, stop);
+        }
+        return "";
+}
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Len(M, N));
