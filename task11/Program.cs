5d19411137fb9e520dc92413aa52bd36fbfd3fa0
SergRas <you@example.com>
2024-04-05 15:19:05 +0500

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Привет мир как дела";
+        string output = ReverseWords(input);
+        
+        Console.WriteLine(output);
+    }
+    
+    static string ReverseWords(string input)
+    {
+        string[] words = input.Split(' ');
+        Array.Reverse(words);
+        
+        return string.Join(" ", words);
+    }
+}
