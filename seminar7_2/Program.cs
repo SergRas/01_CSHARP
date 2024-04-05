673e445e7260d3462c33f38c4f51523418c593c8
SergRas <you@example.com>
2024-04-05 15:17:32 +0500

    add seminar7_2

---
+++ seminar7_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+
+void Consonants(string s)
+{
+    if (s.Length == 0) return;
+    string st = "aoueyi";
+    if (st.Contains(s[0]) == false) Console.Write($"{s[0]}");
+    Consonants(s[1..]);
+}
+
+void Consonants2(string s, int i = 0)
+{
+
+    string st = "aoueyi";
+    if (i == s.Length) return;
+    if (st.Contains(s[i]) == false) Console.Write($"{s[i]}");
+    Consonants2(s, i + 1);
+}
+
+
+string st = Console.ReadLine()!;
+Consonants(st);
+Console.WriteLine();
+Consonants2(st);
