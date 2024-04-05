a74840c983000e156baf646a69868e194bdf9ee7
SergRas <rasulov_sf@mail.ru>
2024-04-06 00:15:16 +0500

    add dz7_3 promatt

---
+++ DZ7_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте произвольный массив. Выведите
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+PrintArrRev(array, array.Length - 1);
+		
+void PrintArrRev(int[] arr, int i)
+    {
+        if (i >= 0)
+        {
+            Console.Write(arr [i] +" ");
+            PrintArrRev(arr, i - 1);
+        }
+    }		
