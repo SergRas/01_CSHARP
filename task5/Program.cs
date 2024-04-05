6721888e9e809dc122c0812221829bec36151552
SergRas <you@example.com>
2024-04-05 15:18:22 +0500

    add task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,10 +1,49 @@
-﻿int n = 100;
-int i = 1;
+﻿using System;
 
-while (i <= n)
+class Program
 {
-    // Console.WriteLine(i);
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
+    static void Main()
+    {
+        char[,] charArray = new char[,]
+        {
+            { 'H', 'e', 'l', 'l', 'o' },
+            { 'W', 'o', 'r', 'l', 'd' }
+        };
+
+        Console.WriteLine("Исходный массив символов:");
+
+        PrintCharArray(charArray);
+
+        string resultString = GetStringFromArray(charArray);
+
+        Console.WriteLine("\nСтрока, созданная из символов массива:");
+        Console.WriteLine(resultString);
+    }
+
+    static void PrintCharArray(char[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static string GetStringFromArray(char[,] array)
+    {
+        string result = "";
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                result += array[i, j];
+            }
+        }
+
+        return result;
+    }
 }
